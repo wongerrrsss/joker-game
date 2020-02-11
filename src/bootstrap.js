fae57eeca2b017e59d1f843db71355193963a37d
TawongaChilongo <tmchilongo92@gmail.com>
2020-02-11 15:55:31 -0500

    bootstrap.js

---
+++ src/bootstrap.js
@@ -0,0 +1,15 @@
+// ES6 code goes here
+// console.log('es6 code goes here');
+class Player {
+    constructor(name) {
+        this.name = name
+
+    }
+}
+
+let result = prompt("Let's play 'Joker', I will explain the game as we go along! First things first, What is your name?")
+    
+const player = Prompt => ("Whazz up (name)?! How much would you like to add to your wallet?!")
+
+
+console.log(player(prompt))
