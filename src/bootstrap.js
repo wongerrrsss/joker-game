356edef2663052cb423c400516dc968bc7c5f284
TawongaChilongo <tmchilongo92@gmail.com>
2020-02-11 22:25:33 -0500

    Introduction to the game

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,6 @@
 // ES6 code goes here
 // console.log('es6 code goes here');
+// INTRODUCTION
 class Player {
     constructor(name) {
         this.name = name
@@ -9,7 +10,7 @@ class Player {
 
 let result = prompt("Let's play 'Joker', I will explain the game as we go along! First things first, What is your name?")
     
-const player = Prompt => ("Whazz up (name)?! How much would you like to add to your wallet?!")
-
-
 console.log(player(prompt))
+
+// WALLET FUNCTION
+const player = Prompt => ("Whazz up (name)?! How much would you like to add to your wallet?!")
