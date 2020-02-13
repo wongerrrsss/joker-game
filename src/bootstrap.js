73266436db5d4caa37b8f3f20bad508167982f3d
TawongaChilongo <tmchilongo92@gmail.com>
2020-02-13 12:49:30 -0500

    update to the game(code is not working)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,59 @@
 // ES6 code goes here
 // console.log('es6 code goes here');
-// INTRODUCTION
+// generate_deck() {
+//     let card = (suit, value) => {
+//         this.name = value + ' of ' + suit
+//         this.suit = suit 
+//         this.value = value 
+
+//         return {name:this.name, suit:this,suit, value:this.value}
+//     }
+
+    // let values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    // let suits = ['Clubs', 'Diamonds', 'Spades', 'Hearts']
+
+//     for ( let s = 0; s < suits.length; s++ ) {
+//         for ( let v = 0; v < values.length; v++) {
+//         this.deck.push(card(suits[s], values[v]))
+//         }
+//     }
+
+// }
+
+// print_deck () {
+//     if (this.deck.length == 0) {
+//         console.log('New Deck in 3..2..1!!!')
+//     } else {
+//         for (let c = 0; c < this.deck.length; c++) {
+//             console.log(this.deck[c])
+//         }
+//     }
+//         }
+//     }
+// }
+
+// }
+
+
+// deck.generate_deck()
+
+
+
+let player = prompt("Let's play 'Joker', I will explain the game as we go along! First things first, What is your name?")
+
+let currentWallet = 0
+
+currentWallet = prompt(`\n"Whazz up ${player}?!\n\nHow much would you like to add to your wallet?! (Since this is a high-stakes game, the minimum is $300)"`);
+alert(`\nDANGGG ${player}?! ${currentWallet}!!! You big ballinnn, hope you brought your A-game! ;)\n\n`)
+
+const cardGame = {
+  let firstCard = 7;
+    const player = {
+      return prompt("Remember everytime you lay a card down $50 gets deducted from your wallet!")}
+      
+    let (firstCard == 8) {
+      alert("\Gertie just played an 8! That means it's her turn again!")}
+
+// Make it so that you are playing against one other person "Gertie"
+
+// console.log(player(prompt))
